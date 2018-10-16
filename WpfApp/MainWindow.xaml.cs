8e77e2dec1e68d7176c90d97a23b102c3f82a193
КимС <@224_10>
2018-10-16 21:07:59 +0600

    wpf

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Xml;
+using System.Windows.Markup;
+using Microsoft.Win32;
+
 
 namespace WpfApp
 {
@@ -23,6 +28,61 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+            // ((Run)FirstParagraph.Inlines.FirstInline).Text = "-*-";
+            Run r = new Run("ewityesi sehfsoieyf ");
+            Paragraph p = new Paragraph(r);
+            ListItem li = new ListItem(p);
+            listhistory.ListItems.Add(li);
+            listhistory.ListItems.Add(new ListItem(new Paragraph(new Run(" ") { })));
+
+            using (FileStream fs = File.Open(@"\\dc\Студенты\ПКО\SEP-172.1\WPF\documGertsen2.xaml", FileMode.Open))
+            {
+                FlowDocument doc = XamlReader.Load(fs) as FlowDocument;
+                if(doc != null)
+                {
+                    dcDocument.Document = doc;
+                }
+                else
+                {
+                    MessageBox.Show("document error ");
+                }
+            }
+
+
+            
+        }
+
+        private void btnSaveDocument_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog openFile = new SaveFileDialog();
+            openFile.Filter = "RichTextBox (*.rtf)|*.rtf|All Files (*.*)|*.*";
+            if (openFile.ShowDialog() == true)
+            {
+                TextRange docTr = new TextRange(
+                    MyRichTextBox.Document.ContentStart,
+                    MyRichTextBox.Document.ContentEnd);
+                using (FileStream fs = File.Create(openFile.FileName))
+                {
+                    docTr.Save(fs, DataFormats.Rtf);
+                }
+            }
+
+        }
+
+        private void btnLoadDocument_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "RichTextBox (*.rtf)|*.rtf|All Files (*.*)|*.*";
+            if(openFile.ShowDialog()==true)
+            {
+                TextRange docTr = new TextRange(
+                    MyRichTextBox.Document.ContentStart,
+                    MyRichTextBox.Document.ContentEnd);
+                using (FileStream fs = File.Open(openFile.FileName, FileMode.Open))
+                {
+                    docTr.Load(fs, DataFormats.Rtf);
+                }
+            }
         }
     }
 }
